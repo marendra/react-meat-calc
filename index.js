ae9283ca3bf2c2fb7700179ad23af9e81d866e2a
marendra <marendra79@yahoo.com>
2019-07-02 12:17:27 +0700

    2 files modified

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import Icon from '@material-ui/core/Icon';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -18,25 +24,39 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+   stickToBottom: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+  },
 }));
 
  const App = function() {
 
     const classes = useStyles();
+    const [value, setValue] = React.useState('recents');
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
+             <Typography variant="h6" className={classes.title}>
+            Meat Calc
           </Typography>
-          <Button color="inherit">Login</Button>
+         
         </Toolbar>
       </AppBar>
+
+       <BottomNavigation value={value} onChange={handleChange} className={classes.stickToBottom}>
+      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Folder" value="folder" icon={<Icon>folder</Icon>} />
+    </BottomNavigation>
     </div>
   );
   
