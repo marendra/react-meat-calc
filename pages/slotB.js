6d030231efa165776d72263f6d26e65a59cf470e
marendra <marendra79@yahoo.com>
2019-07-04 12:36:41 +0700

    3 files modified

---
+++ pages/slotB.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function slotB(){
+  return(
+    <h1>SlotB</h1>
+  )
+}
---
+++ pages/utama.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default function utama (){
+  return (
+    <h1>Home</h1>
+  )
+}
