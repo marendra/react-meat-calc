a023c8f6909dcb83ecb609d42a3c17afa275d3e8
marendra <marendra79@yahoo.com>
2019-07-04 12:38:00 +0700

    2 files modified

---
+++ pages/slotC.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function slotC(){
+  return(
+    <h1>SlotC</h1>
+  )
+}
---
+++ pages/slotD.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function slotD(){
+  return(
+    <h1>SlotD</h1>
+  )
+}
