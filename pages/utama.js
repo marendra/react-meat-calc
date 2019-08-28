122d3924079574e78c126e4393d1d8a5ebf87436
marendra <marendra79@yahoo.com>
2019-08-29 06:22:25 +0700

    1 file modified

--- pages/utama.js
+++ pages/utama.js
@@ -5,7 +5,10 @@ export default function utama (){
   return (
     <div>
     <h1>Home</h1>
-    <button onClick={Firebase.login}>Login</button>
+    <button onClick={login}>Login</button>
  </div>
   )
+  async function login (){
+    await Firebase.login 
+  }
 }
